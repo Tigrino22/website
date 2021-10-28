ad9f26da1fa88ce7465ab97f7fa318f1d5ed6ee9
Valentin Legris <valentin.legris@outlook.com>
2021-10-28 23:43:49 +0200

    first commit

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'app_admin_')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+            'current_menu' => 'app_admin'
+        ]);
+    }
+}
